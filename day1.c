e189c0ea5bbbb1f92668cd0443a7aa7afc1f074a
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-09 13:46:00 +0545

    day2

--- day1.c
+++ day1.c
@@ -1,68 +1,29 @@
-#include <stdio.h>
 
+//document section
+
+/*
+Author: Sandis Prajapati
+Date: 04/09/2024
+Purpose: This program is used to print "Hello World"
+version: 1.0
+*/
+
+
+//defination section
+#define pi 3.114
+
+//global section
+int a = 10;
+
+//main function
 int main(){
-      //makes variable read-only
-        // const int c = 50;
-      
-
-        // printf("%d", c);
-    
-   /*
-   1. Cannot use keyword - 32 keywords
-   2. cannot start with numeric values - 1a,2a
-   3. cannot use blank space and comma (,)
-   4. cannot use special character (%,&,^) 
-        except underscore(_)
-    5. case sensitive (A and a are different)
-    6. variable must be unique.
-
-    apple and APPLE are different
-    
   
-   */
+    printf("Hello World\n");
 
-   //Data types
-   //int - 50, 100, 20
-   //double - 50.205
-   //float - 50.205
-   //char - "M"
-
-   //void
-
-
- /*
-    format specifier
-    int - "%d"
-    float - "%f"
-    char - "c"
-
-    
-   int a = 9;
-    int x = 10;
-    int y = 20;
-
-    float b = 2.5;
-
-    char c = "M";
-
-    printf("%d,%f,%c",a,b,c);
-
-
-    */
-
-
-  int a;
-  int b;
-  printf("Enter first number: ");
-  //scanf is used for user input
-  scanf("%d",&a);
-  
-  printf("Enter second number: ");
-  scanf("%d",&b);
-  
-  int c = a+b;
-
-  printf("The sum is %d",c);
-
-   
 }
+
+//sub-function
+int sum(){
+
+}
+
---
+++ day2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main(){
+      //makes variable read-only
+        // const int c = 50;
+      
+
+        // printf("%d", c);
+    
+   /*
+   1. Cannot use keyword - 32 keywords
+   2. cannot start with numeric values - 1a,2a
+   3. cannot use blank space and comma (,)
+   4. cannot use special character (%,&,^) 
+        except underscore(_)
+    5. case sensitive (A and a are different)
+    6. variable must be unique.
+
+    apple and APPLE are different
+    
+  
+   */
+
+   //Data types
+   //int - 50, 100, 20
+   //double - 50.205
+   //float - 50.205
+   //char - "M"
+
+   //void
+
+
+ /*
+    format specifier
+    int - "%d"
+    float - "%f"
+    char - "c"
+
+    
+   int a = 9;
+    int x = 10;
+    int y = 20;
+
+    float b = 2.5;
+
+    char c = "M";
+
+    printf("%d,%f,%c",a,b,c);
+
+
+    */
+
+
+  int a;
+  int b;
+  printf("Enter first number: ");
+  //scanf is used for user input
+  scanf("%d",&a);
+  
+  printf("Enter second number: ");
+  scanf("%d",&b);
+  
+  int c = a+b;
+
+  printf("The sum is %d",c);
+
+   
+}
