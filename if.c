5316839133b32459f30cd19286aeb484f618e36d
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-11 13:20:16 +0545

    if else

---
+++ if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter your age:");
+    scanf("%d",&a);
+
+    
+    if(a >= 18 && age < 50){
+        printf("You can drive");
+    }else{
+        printf("You cannot drive");
+    }
+
+    /*
+    Decision control statement
+    if, if else,
+     else if ladder, 
+     nested if
+    */
+
+
+}
