d91ca2a99ed4dc6256a3fd1ce02c43c76adf2ec5
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-20 13:54:57 +0545

    function task

--- function_task.c
+++ function_task.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+//function declaration
+int addition(int,int);
+int subtraction(int,int);
+void multiplication(int,int);
+
 int main(){
     int first,second,choice;
+    int i = 1;
+    while(i){
+        
     printf("Choose\n");
     printf("1.Addition 2.Subtraction 3.Multiplication\n");
     scanf("%d",&choice);
@@ -14,5 +22,27 @@ int main(){
     }else{
         printf("Invalid choice");
     }
-    
+
+    if(choice == 1){
+        int sum = addition(first,second);
+        printf("The sum is %d\n",sum);
+    }else if(choice == 2){
+        int sub = subtraction(first,second);
+            printf("The subtraction is %d\n",sub);
+    }else if(choice == 3){
+         multiplication(first,second);
+    }else{}
+    }
+}
+
+int addition(int a,int b){
+    return a+b;
+}
+
+int subtraction(int a,int b){
+    return a-b;
+}
+
+void multiplication(int a,int b){
+    printf("The multiplication is %d\n",a*b);
 }
