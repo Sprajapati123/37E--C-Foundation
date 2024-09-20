c637ee1ae26897ac43016269f4ec38cd5c1bd4fb
sprajapati123 <sandisprajapati52@gmail.com>
2024-09-20 12:58:55 +0545

    functions

---
+++ functions.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+//function declaration
+void display();
+void greet();
+
+int main() {
+    //calling function
+    display();
+    greet();
+}
+//function defination
+void display(){
+    printf("Hello world\n");
+}
+
+void greet(){
+    printf("Good Afternoon")
+}
+/*
+ 1. Function with no parameter no return type
+ 2. Function with parameter but no return type
+ 3. Function with no parameter but return type
+ 4. Function with parameter and return type
+*/
